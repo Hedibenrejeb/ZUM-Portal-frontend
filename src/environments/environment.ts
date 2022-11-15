c32bdb91d36dc8104295b8b68ba04986ab349e3d
unknown <nesrouna36@gmail.com>
2022-11-15 15:53:02 +0100

    f

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,33 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-
   production: false,
-
   apiUrl:'http://localhost:8000',
-
   defaultauth: 'fackbackend',
-
   firebaseConfig: {
-
     apiKey: '',
-
     authDomain: '',
-
     databaseURL: '',
-
     projectId: '',
-
     storageBucket: '',
-
     messagingSenderId: '',
-
     appId: '',
-
     measurementId: ''
-
   }
-
 };
 
 
